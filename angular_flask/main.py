155717468d6767db205c38802a10065d8cfdebee
ray-curran <rpcurr@gmail.com>
2016-07-18 00:14:20 -0500

    Remove 2 second delay for timeout issue

--- angular_flask/main.py
+++ angular_flask/main.py
@@ -116,7 +116,7 @@ def api_req(api_endpoint, access_token, *mehs, **kw):
             print("\n\n")
 
         print("Sleeping for 2 seconds to get around rate-limit.")
-        time.sleep(2)
+        # time.sleep(2)
         return p_ret
     except Exception, e:
         if DEBUG:
